5db32ca8e17406daa05511ed307a294de7649951
Fillion  Adam <adamfillion>
2018-03-19 15:57:01 -0400

    3-19-2018

--- v0.10_AutomaticGuitarTuner/v0.10_AutomaticGuitarTuner.ino
+++ v0.10_AutomaticGuitarTuner/v0.10_AutomaticGuitarTuner.ino
@@ -116,8 +116,8 @@ bool error_flag = 0;
 //CONSIDER SHRINKING THIS ONTO ONE LINE IE. struct strings E2, AA2, D3, G3 etc...
 struct strings  {
   double targetFrequency;
-  double tighten_rate; //the frequency shift for a quarter rotation of the adjustment knob
-  double loosen_rate;
+  double rate; //the frequency shift for a quarter rotation of the adjustment knob
+  int label;
   int ServoPin;
 };
 struct strings E2;
@@ -139,28 +139,40 @@ void setup()
  
 ///////////////DEFINE STRING PARAMETERS /////////////////////////////////
   E2.targetFrequency = 82.4; //desired 'in tune' pitch
-  E2.loosen_rate= 0.04; //how fast the adjustment knob reduces the frequency in Hz/degree of rotation (loosen)
-  E2.tighten_rate= 0.03;
+  E2.rate= 0.04; //how fast the adjustment knob reduces the frequency in Hz/degree of rotation (loosen) 
   E2.ServoPin = 10;
+  E2.label = 1;
+  
   AA2.targetFrequency = 110;
-  AA2.loosen_rate= 0.0511;  
-  AA2.tighten_rate = 0.0844;
+  AA2.rate= 0.0511;  
   AA2.ServoPin = 11;
+  AA2.label = 2;
+  
   D3.targetFrequency = 146.8;
-  D3.loosen_rate= 0.05;  
-  D3.tighten_rate = 0.0722;
+  D3.rate= 0.05;  
+  D3.ServoPin = 12;
+  D3.label = 3;
+  
   G3.targetFrequency = 196;
-  G3.loosen_rate = 0.07; 
-  G3.tighten_rate = 0.03666;
+  G3.rate = 0.07; 
+  G3.ServoPin = 13;
+  G3.label = 4;
+  
   B3.targetFrequency = 247;
-  B3.loosen_rate = 0.094;
-  B3.tighten_rate = 0.128;
+  B3.rate = 0.094;
+  B3.ServoPin = 14;
+  B3.label = 5;
+  
+  
   E4.targetFrequency = 329.6;
-  E4.loosen_rate = 0.08888;  
-  E4.tighten_rate= 0.084;
+  E4.rate = 0.08888;  
+  E4.ServoPin = 15;
+  E4.label = 6;
+  
   tuneString.targetFrequency = 100;
-  tuneString.loosen_rate = 100;
-  tuneString.tighten_rate = 100;
+  tuneString.rate = 100;
+  tuneString.ServoPin = 10;
+  tuneString.label = 122;
 
   //USER INTERFACE LEDS//
   pinMode(set_pin, INPUT);
@@ -171,15 +183,6 @@ void setup()
   pinMode(LED_OUTPUT_STRING_B2, OUTPUT);
   pinMode(LED_OUTPUT_STRING_E2, OUTPUT);
 
-
-
-  
-
-
-
-
-   
-  
 
 }
 ///////////////////////END CONFIGURATION AND SETUP/////////////////////////
@@ -273,8 +276,6 @@ if (averageArray(vReal)>9) {
    no_Signal = 0;   //Serial.println(averageArray(vReal));
 } else {
   no_Signal = 1;
-  //Serial.println("Insufficient Signal");
- //Serial.println(averageArray(vReal));
 }
 
 //*****************Enter Algorithm********************//
@@ -295,37 +296,49 @@ void complete_algorithm(double* vReal, double* vImag, uint16_t samples ){
 if (digitalRead(LED_OUTPUT_STRING_E2) == HIGH){
   //Serial.println("e2 selected");
    tuneString.targetFrequency = E2.targetFrequency;
-   tuneString.loosen_rate = E2.loosen_rate; //how fast the adjustment knob reduces the frequency in Hz/degree of rotation (loosen)
-   tuneString.tighten_rate = E2.tighten_rate;
+   tuneString.rate = E2.rate; //how fast the adjustment knob reduces the frequency in Hz/degree of rotation (loosen)
+   tuneString.label = E2.label;
    servoTune.attach(E2.ServoPin); //must be a pwm pin
    for (int c = 0; c<samples; c++) {
       vReal[c] = butterWorth_E2(vReal[c]);
     }
 }else if  (digitalRead(LED_OUTPUT_STRING_A2) == HIGH){
   tuneString.targetFrequency = AA2.targetFrequency;
-  tuneString.loosen_rate = AA2.loosen_rate; //how fast the adjustment knob reduces the frequency in Hz/degree of rotation (loosen)
-   tuneString.tighten_rate = AA2.tighten_rate;
+  tuneString.rate = AA2.rate; //how fast the adjustment knob reduces the frequency in Hz/degree of rotation (loosen)
+  tuneString.label = AA2.label;
   servoTune.attach(AA2.ServoPin); //must be a pwm pin
    for (int c = 0; c<samples; c++) {
       vReal[c] = butterWorth_A2(vReal[c]);
    }
 } else if  (digitalRead(LED_OUTPUT_STRING_D3) == HIGH){
-  tuneString.targetFrequency = D3.targetFrequency;;
+  tuneString.targetFrequency = D3.targetFrequency;
+  tuneString.rate = D3.rate; //how fast the adjustment knob reduces the frequency in Hz/degree of rotation (loosen)
+  tuneString.label = D3.label;
+  servoTune.attach(D3.ServoPin); //must be a pwm pin
    for (int c = 0; c<samples; c++) {
       vReal[c] = butterWorth_D3(vReal[c]);
    }
 } else if  (digitalRead(LED_OUTPUT_STRING_G3) == HIGH){
   tuneString.targetFrequency = G3.targetFrequency;
+  tuneString.rate = G3.rate; //how fast the adjustment knob reduces the frequency in Hz/degree of rotation (loosen)
+  tuneString.label = G3.label;
+  servoTune.attach(G3.ServoPin); //must be a pwm pin
    for (int c = 0; c<samples; c++) {
       vReal[c] = butterWorth_G3(vReal[c]);
    }
 } else if  (digitalRead(LED_OUTPUT_STRING_B2) == HIGH){
   tuneString.targetFrequency = B3.targetFrequency;
+  tuneString.rate = B3.rate; //how fast the adjustment knob reduces the frequency in Hz/degree of rotation (loosen)
+  tuneString.label = B3.label;
+  servoTune.attach(B3.ServoPin); //must be a pwm pin
    for (int c = 0; c<samples; c++) {
       vReal[c] = butterWorth_B2(vReal[c]);
    }
 } else if  (digitalRead(LED_OUTPUT_STRING_E4) == HIGH){
   tuneString.targetFrequency = E4.targetFrequency;
+  tuneString.rate = E4.rate; //how fast the adjustment knob reduces the frequency in Hz/degree of rotation (loosen)
+  tuneString.label = E4.label;
+  servoTune.attach(E4.ServoPin); //must be a pwm pin
    for (int c = 0; c<samples; c++) {
       vReal[c] = butterWorth_E4(vReal[c]);
    }
@@ -412,25 +425,24 @@ if (abs(error)<1.5){
   is_Tuned = 0;
   Serial.println("Guitar NOT TUNED");
   //motor control
-  if (error<0) {
+/*  if (error<0) {
     //loosen
     
-    
-    rightRotate(abs(error)/tuneString.loosen_rate, servoTune);
+    rightRotate(abs(error)/tuneString.rate, servoTune);
     
   }else if (error>0) {
     //tighten
-    leftRotate(abs(error)/tuneString.tighten_rate, servoTune);
+    leftRotate(abs(error)/tuneString.rate, servoTune);
     
     
   }else {
     Serial.println("Error");
   }
+  */
 
-
-
-
+  smartRotate(abs(error)/tuneString.rate,servoTune, tuneString.label, error);
   
+
 }else {
   is_Tuned = 0;
   error_flag = 1;
@@ -440,18 +452,8 @@ if (abs(error)<1.5){
 
 }
 
-///////Average function/////////
-double averageArray(double* vReal){
-double sum = 0;
-  for (int i = 0; i<sizeof(vReal);i++){
-    sum += vReal[i];   
-  }
-  return (sum/sizeof(vReal));
-}
 
-void adjustString(double error) {
-  
-}
+
 
 
 //SERVO COMMANDS//////////
@@ -474,6 +476,62 @@ double rightRotate(double angle, Servo servo){
 }
 
 
+////////////////////////NEW FUNCTIONS THAT CAN DETECT WHICH STRING IS SELECTED /////////////////////
+void smartRotate(double angle, Servo servoTune, int label, double error){
+
+ if (error<0) {
+  switch (label) {
+    case 1: //E2
+      rightRotate(abs(error)/tuneString.rate, servoTune);
+      break;
+    case 2: //A2
+      rightRotate(abs(error)/tuneString.rate, servoTune);
+      break;
+    case 3: //D3
+      rightRotate(abs(error)/tuneString.rate, servoTune);
+      break;
+    case 4: //G3
+      rightRotate(abs(error)/tuneString.rate, servoTune);
+      break;
+    case 5: //B2
+      rightRotate(abs(error)/tuneString.rate, servoTune);
+      break;
+    case 6: //E4
+      rightRotate(abs(error)/tuneString.rate, servoTune);
+      break;
+    default:
+      Serial.println("No String Selected");
+      break;
+  }
+ } else if (error>0) {
+      switch (label) {
+    case 1: //E2
+      leftRotate(abs(error)/tuneString.rate, servoTune);
+      break;
+    case 2: //A2
+      leftRotate(abs(error)/tuneString.rate, servoTune);
+      break;
+    case 3: //D3
+      leftRotate(abs(error)/tuneString.rate, servoTune);
+      break;
+    case 4: //G3
+      leftRotate(abs(error)/tuneString.rate, servoTune);
+      break;
+    case 5: //B2
+      leftRotate(abs(error)/tuneString.rate, servoTune);
+      break;
+    case 6: //E4
+      leftRotate(abs(error)/tuneString.rate, servoTune);
+      break;
+    default:
+      Serial.println("No String Selected");
+      break;
+  }
+ }
+
+ 
+}
+
 //LEFT ROTATE
 //Left rotate
 double leftRotate(double angle, Servo servo){
@@ -489,5 +547,14 @@ double leftRotate(double angle, Servo servo){
     servo.writeMicroseconds(1500);
 
   }
- 
+}
+
+
+///////Average function/////////
+double averageArray(double* vReal){
+double sum = 0;
+  for (int i = 0; i<sizeof(vReal);i++){
+    sum += vReal[i];   
+  }
+  return (sum/sizeof(vReal));
 }
